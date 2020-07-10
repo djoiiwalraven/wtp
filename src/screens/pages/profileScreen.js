92af78b8bd3d78acb9d099072fd6259ea631d631
Djoï Walraven <ddwalraven@Djois-MBP.fritz.box>
2020-07-10 16:47:45 +0200

    updated Profile page

--- src/screens/pages/profileScreen.js
+++ src/screens/pages/profileScreen.js
@@ -6,19 +6,64 @@ import {
   Button,
 } from 'react-native';
 
+import IoButton from '../../objects/dumb/buttons/ioButton';
+
 export default function ProfileScreen(props) {
   return(
     <View style={styles.inputContainer}>
-      <Button title='sign out' onPress={props.signOut} />
-      <Button title='create new event' onPress={props.goEventScreen} />
+      <View style={styles.infoContainer}>
+        <Text style={styles.title}>Personal Information</Text>
+        <Text style={styles.info}>user: ddwalraven</Text>
+        <Text style={styles.info}>@: dd.walraven@gmail.com</Text>
+        <Text style={styles.info}>Tell: +31637401229</Text>
+        <Text style={styles.title}>Information we share</Text>
+        <Text style={styles.info}>Djoï Walraven</Text>
+        <Text style={styles.info}>Cake: 02/09/1997</Text>
+        <Text style={styles.info}>Arrow: Male</Text>
+        <Text style={styles.info}>Speaks: English & Dutch</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <IoButton styling={{backgroundColor: '#fff'}} title='sign out' click={props.signOut} />
+        <IoButton styling={{backgroundColor: '#fff'}} title='create new event' click={props.goEventScreen} />
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
-    marginLeft: 20,
-    marginRight: 20,
     height: '100%',
+    alignItems: 'center',
+  },
+  infoContainer: {
+    flex: 3,
+    alignItems: 'flex-start',
+    width: '80%',
+  },
+  buttonContainer: {
+    flex: 1,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    marginTop: 36,
+    marginBottom: 12,
+    fontWeight: 'bold',
+  },
+  info: {
+    fontSize: 18,
+    marginBottom: 5,
   }
+
 });
+//PROFILE       //PAYMENT METHOD
+//Full name     //PayPal
+//Username      //iDeal
+//Email         //CreditCard
+//Tell          //
+//Birthday      //
+//Sex           //
+//Language      //
+//Location      //
